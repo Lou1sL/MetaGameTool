bf80718f76b5c955d71f6704bb8f54fe40a3e9fc
留白 <156890093@qq.com>
2018-07-20 17:56:00 +0800

    yooooo~

---
+++ CrazyInput.cs
@@ -0,0 +1,269 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Windows.Forms;
+using UnityStandardUtils.Extension;
+using System.Drawing;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System;
+using UnityStandardUtils;
+
+public class CrazyInput : SingletonMonoBehaviour<CrazyInput>
+{
+
+    public enum MouseClick
+    {
+        Click, ClickDown, ClickUp
+    }
+    public class InputAction
+    {
+        private enum ActType
+        {
+            MouseMove, MouseClick, Keyboard,Action
+        }
+
+        private ActType ActT;
+        private float WaitTimeCounter;
+        private bool IsWaiting = false;
+
+        private MouseClick Click;
+        private Transform ToPosition;
+        private string KBKey;
+        private Action Act;
+
+        public InputAction(float waitT)
+        {
+            IsWaiting = true;
+            WaitTimeCounter = waitT;
+        }
+        public InputAction(string key,float waitT = 0.2f)
+        {
+            WaitTimeCounter = waitT;
+            ActT = ActType.Keyboard;
+            KBKey = key;
+        }
+        public InputAction(Transform p)
+        {
+            ActT = ActType.MouseMove;
+            ToPosition = p;
+        }
+        public InputAction(MouseClick mc,float waitT = 0.5f)
+        {
+            WaitTimeCounter = waitT;
+            ActT = ActType.MouseClick;
+            Click = mc;
+        }
+        public InputAction(Action a,float waitT = 1f)
+        {
+            WaitTimeCounter = waitT;
+            ActT = ActType.Action;
+            Act = a;
+        }
+
+        public bool Action()
+        {
+            if (IsWaiting) return Waiting();
+
+
+            switch (ActT)
+            {
+                case ActType.MouseMove:
+                    return LerpMove(ToPosition.position);
+
+                case ActType.MouseClick:
+                    switch (Click)
+                    {
+                        case MouseClick.Click: LeftClick(); break;
+                        case MouseClick.ClickDown: LeftClickDown(); break;
+                        case MouseClick.ClickUp: LeftClickUp(); break;
+                    }
+                    break;
+
+                case ActType.Keyboard:
+                    SendKeys.SendWait(KBKey);
+                    break;
+
+                case ActType.Action:
+                    Act.Invoke();
+                    break;
+            }
+            IsWaiting = true;
+            
+            return false;
+        }
+        private bool Waiting()
+        {
+            WaitTimeCounter -= Time.deltaTime;
+            return WaitTimeCounter < 0;
+        }
+    }
+    private static List<InputAction> ProceduralAction = new List<InputAction>();
+    
+    private static MetaGameTool.Task.WindowStruct GameWindow;
+    private void Start()
+    {
+        GameWindow = MetaGameTool.Task.Window.GetWindow(Process.GetCurrentProcess());
+        
+
+        //TEST
+        ProceduralAction = new List<InputAction>()
+        {
+            new InputAction(5),
+            new InputAction(()=>{ AI2DTargetManagerSingleton.Instance.Player.GetComponent<CC2DBackpackUserInput>().Opened = true; }),
+            new InputAction(AI2DTargetManagerSingleton.Instance.Player.GetComponent<PlayerBackpackUI>().ObjSelector.transform),
+            new InputAction(MouseClick.Click),
+            new InputAction(AI2DTargetManagerSingleton.Instance.Player.GetComponent<PlayerBackpackUI>().Drop.transform),
+            new InputAction(MouseClick.Click),
+            new InputAction(()=>{ AI2DTargetManagerSingleton.Instance.Player.GetComponent<CC2DBackpackUserInput>().Opened = false; }),
+        };
+
+
+    }
+
+    private void Update()
+    {
+        if (ProceduralAction.Count <= 0) return;
+
+        InputAction act = ProceduralAction[0];
+        if (act.Action()) ProceduralAction.RemoveAt(0);
+    }
+    
+
+
+    /// <summary>
+    /// World V3 To View V3
+    /// </summary>
+    /// <param name="w"></param>
+    /// <returns></returns>
+    private static Vector2 W2V(Vector3 w)
+    {
+        return Camera.main.WorldToScreenPoint(w);
+    }
+    /// <summary>
+    /// View V3 To World V3
+    /// </summary>
+    /// <param name="v"></param>
+    /// <returns></returns>
+    private static Vector2 V2W(Vector2 v)
+    {
+        return VectorExtension.ScreenToWorldPointPerspective(v);
+    }
+    /// <summary>
+    /// V3 To Position
+    /// </summary>
+    /// <param name="v"></param>
+    /// <returns></returns>
+    private static Point V2P(Vector3 v)
+    {
+        return new Point((int)v.x, UnityEngine.Screen.currentResolution.height - (int)v.y - 1);
+    }
+    /// <summary>
+    /// Position To V3
+    /// </summary>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    private static Vector2 P2V(Point p)
+    {
+        return new Vector2(p.X, UnityEngine.Screen.currentResolution.height - p.Y - 1);
+    }
+    
+    /// <summary>
+    /// 鼠标在屏幕的坐标
+    /// </summary>
+    public static Vector2 MousePosition
+    {
+        get { return P2V(System.Windows.Forms.Cursor.Position); }
+        set { System.Windows.Forms.Cursor.Position = V2P(value); }
+    }
+    /// <summary>
+    /// 鼠标在游戏界面的坐标
+    /// </summary>
+    public static Vector2 MouseLocalPosition
+    {
+        get { return Input.mousePosition; }
+        set { MousePosition = (MousePosition - (Vector2)Input.mousePosition) + value; }
+    }
+    /// <summary>
+    /// 鼠标在游戏世界的坐标（get：Z取0时）
+    /// </summary>
+    public static Vector3 MouseWorldPosition
+    {
+        get { return VectorExtension.ScreenToWorldPointPerspective(MouseLocalPosition); }
+        set { MouseLocalPosition = Camera.main.WorldToScreenPoint(value); }
+    }
+
+    /// <summary>
+    /// 移动目标位置的百分比距离
+    /// </summary>
+    /// <param name="FinalWorldPosition">目标位置</param>
+    /// <param name="p">移动百分比</param>
+    /// <returns></returns>
+    public static bool LerpMove(Vector3 FinalWorldPosition, float p = 0.2f)
+    {
+        bool xArrive = true;
+        bool yArrive = true;
+
+        Vector2 dis = (Vector2)Camera.main.WorldToScreenPoint(FinalWorldPosition) - MouseLocalPosition;
+
+        if (Mathf.Abs(dis.x) > 5f)
+        {
+            dis.x *= p;
+            xArrive = false;
+        }
+        if (Mathf.Abs(dis.y) > 5f)
+        {
+            dis.y *= p;
+            yArrive = false;
+        }
+
+        MouseLocalPosition = dis + MouseLocalPosition;
+        return (xArrive && yArrive);
+    }
+
+    /// <summary>
+    /// 鼠标单击
+    /// </summary>
+    public static void LeftClick() { MetaGameTool.Task.Mouse.Click(); }
+    /// <summary>
+    /// 鼠标左键按下
+    /// </summary>
+    public static void LeftClickDown() { MetaGameTool.Task.Mouse.ClickDown(); }
+    /// <summary>
+    /// 鼠标左键抬起
+    /// </summary>
+    public static void LeftClickUp() { MetaGameTool.Task.Mouse.ClickUp(); }
+    
+    /// <summary>
+    /// 游戏界面窗口信息
+    /// </summary>
+    public static Rect GameWindowRect
+    {
+        get { return MetaGameTool.Task.Window.GetWindowRect(GameWindow.MainWindowHandle); }
+    }
+    
+    /// <summary>
+    /// 清理队列事件
+    /// </summary>
+    public static void ClrProceduralAction()
+    {
+        ProceduralAction.Clear();
+    }
+    /// <summary>
+    /// 设置队列事件
+    /// </summary>
+    /// <param name="list"></param>
+    public static void SetProceduralAction(List<InputAction> list)
+    {
+        ProceduralAction.Clear();
+        ProceduralAction = list;
+    }
+    /// <summary>
+    /// 添加一个事件到队列
+    /// </summary>
+    /// <param name="ca"></param>
+    public static void AddProceduralAction(InputAction ca)
+    {
+        ProceduralAction.Add(ca);
+    }
+}
---
+++ MetaGameTool.cs
@@ -0,0 +1,467 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+using UnityStandardUtils.Extension;
+using UnityStandardUtils.Web;
+
+namespace UnityStandardUtils
+{
+
+
+    public static class MetaGameTool
+    {
+
+        /// <summary>
+        /// 路径
+        /// </summary>
+        public static class Path
+        {
+            public static readonly string SystemPath = Environment.GetFolderPath(Environment.SpecialFolder.System);
+            public static readonly string DesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            public static readonly string StreamingAssetsPath = Application.streamingAssetsPath;
+            public static readonly string GameDataPath = Application.dataPath;
+        }
+        /// <summary>
+        /// 文件操作相关
+        /// </summary>
+        public static class File
+        {
+            /// <summary>
+            /// 从StreamingAssets复制
+            /// </summary>
+            /// <param name="oriname">源文件名</param>
+            /// <param name="targetname">目标文件名（留空为不改名）</param>
+            /// <param name="targetpath">目标路径（留空为桌面路径）</param>
+            /// <returns>是否成功</returns>
+            public static bool CopyFromSA(string oriname, string targetname = null, string targetpath = null)
+            {
+                string oripath = System.IO.Path.Combine(Application.streamingAssetsPath, oriname);
+                string topath = System.IO.Path.Combine(targetpath ?? Path.DesktopPath, targetname ?? oriname);
+
+                if (System.IO.File.Exists(topath) || !System.IO.File.Exists(oripath)) return false;
+
+
+                System.IO.File.Copy(oripath, topath);
+
+                return true;
+            }
+            /// <summary>
+            /// 删除文件（小心！）
+            /// </summary>
+            /// <param name="name">文件名</param>
+            /// <param name="path">路径(留空为StreamingAssets路径)</param>
+            /// <returns></returns>
+            public static bool DeleteFile(string name, string path = null)
+            {
+
+                string rmpath = System.IO.Path.Combine(path ?? Path.StreamingAssetsPath, name);
+
+                if (!System.IO.File.Exists(rmpath)) return false;
+
+                System.IO.File.Delete(rmpath);
+
+                return true;
+            }
+            /// <summary>
+            /// 读取字符文件
+            /// </summary>
+            /// <param name="name">文件名</param>
+            /// <param name="path">路径（留空为桌面路径）</param>
+            /// <returns></returns>
+            public static string ReadFile(string name, string path = null)
+            {
+                string p = path ?? Path.DesktopPath;
+                p = System.IO.Path.Combine(p, name);
+
+                StreamReader sr = System.IO.File.OpenText(p);
+                return sr.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// 进程相关
+        /// </summary>
+        public static class Task
+        {
+            /// <summary>
+            /// 运行控制台程序，并返回控制台输出，会卡住游戏主进程
+            /// </summary>
+            /// <param name="process">程序名</param>
+            /// <param name="args">进程参数</param>
+            /// <param name="path">路径(留空为StreamingAssets路径)</param>
+            /// <returns>控制台输出</returns>
+            public static string RunProcess(string process, string args = null, string path = null)
+            {
+                string p = path ?? Path.StreamingAssetsPath;
+                p = System.IO.Path.Combine(p, process);
+                if (!System.IO.File.Exists(p)) return null;
+
+                Process proc = new Process();
+
+                proc.EnableRaisingEvents = false;
+                proc.StartInfo.FileName = p;
+                if (args != null) proc.StartInfo.Arguments = args;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.Start();
+                string fingerprint = proc.StandardOutput.ReadLine();
+                proc.WaitForExit();
+                return fingerprint;
+            }
+            
+            public class WindowStruct
+            {
+                public string Title { get; set; }
+                public IntPtr MainWindowHandle { get; set; }
+            }
+            public class Window
+            {
+                private delegate bool CallBackPtr(int hwnd, int lParam);
+                private static CallBackPtr callBackPtr = Callback;
+                private static List<WindowStruct> _WinStructList = new List<WindowStruct>();
+
+                [DllImport("user32.dll")]
+                [return: MarshalAs(UnmanagedType.Bool)]
+                private static extern bool EnumWindows(CallBackPtr lpEnumFunc, IntPtr lParam);
+
+                [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+                private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+                [DllImport("user32.dll")]
+                private static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+                
+                [DllImport("user32.dll", EntryPoint = "GetWindowRect")]
+                private static extern int GetWindowRect(IntPtr hwnd, out Rectangle rect);
+
+                [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+                private static extern bool SetWindowPos(IntPtr hwnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+
+                private static bool Callback(int hWnd, int lparam)
+                {
+                    StringBuilder sb = new StringBuilder(256);
+                    int res = GetWindowText((IntPtr)hWnd, sb, 256);
+                    _WinStructList.Add(new WindowStruct { MainWindowHandle = (IntPtr)hWnd, Title = sb.ToString() });
+                    return true;
+                }
+                /// <summary>
+                /// 获取全部窗口
+                /// </summary>
+                /// <returns></returns>
+                public static List<WindowStruct> GetWindows()
+                {
+                    _WinStructList = new List<WindowStruct>();
+                    EnumWindows(callBackPtr, IntPtr.Zero);
+                    return _WinStructList;
+                }
+                /// <summary>
+                /// 获取进程窗口
+                /// </summary>
+                /// <param name="proc"></param>
+                /// <returns></returns>
+                public static WindowStruct GetWindow(Process proc)
+                {
+                    foreach (WindowStruct ws in GetWindows())
+                    {
+                        uint pid = 0;
+                        IntPtr p = GetWindowThreadProcessId(ws.MainWindowHandle, out pid);
+                        if (proc.Id == pid) return ws;
+                    }
+                    return null;
+                }
+
+                /// <summary>
+                /// 设置窗口的Rect
+                /// </summary>
+                /// <param name="window"></param>
+                /// <param name="rect"></param>
+                public static void SetWindowRect(IntPtr window, Rect rect)
+                {
+                    if (rect.size == Vector2.zero)
+                        SetWindowPos(window, 0, (int)rect.position.x, (int)rect.position.y + 1, 0, 0, 1);
+                    else
+                        SetWindowPos(window, 0, (int)rect.position.x, (int)rect.position.y + 1, (int)rect.size.x, (int)rect.size.y, 0);
+                }
+                /// <summary>
+                /// 获取窗口的Rect
+                /// </summary>
+                /// <param name="window"></param>
+                /// <returns></returns>
+                public static  Rect GetWindowRect(IntPtr window)
+                {
+                    Rectangle rect = new Rectangle();
+                    GetWindowRect(window, out rect);
+
+                    Rect r = new Rect();
+                    r.position = new Vector2(rect.X, UnityEngine.Screen.height - rect.Y - 1);
+                    r.size = new Vector2(rect.Width - rect.X, rect.Height - rect.Y);
+                    return r;
+                }
+            }
+            public class Mouse
+            {
+                [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+                private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+                //Mouse actions
+                private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+                private const int MOUSEEVENTF_LEFTUP = 0x04;
+                private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+                private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+                private static void _click(uint action)
+                {
+                    uint X = (uint)System.Windows.Forms.Cursor.Position.X;
+                    uint Y = (uint)System.Windows.Forms.Cursor.Position.Y;
+                    mouse_event(action, X, Y, 0, 0);
+                }
+
+                public static void Click()
+                {
+                    _click(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP);
+                }
+
+                public static void ClickDown()
+                {
+                    _click(MOUSEEVENTF_LEFTDOWN);
+                }
+
+                public static void ClickUp()
+                {
+                    _click(MOUSEEVENTF_LEFTUP);
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 吓人的玩意
+        /// </summary>
+        public static class Mad
+        {
+            /// <summary>
+            /// CRASH!
+            /// </summary>
+            public static void GameCrash()
+            {
+                for (; ; );
+            }
+            /// <summary>
+            /// NOOOOOOOOO!
+            /// Be careful
+            /// </summary>
+            public static void BSOD()
+            {
+                Task.RunProcess("BSOD.exe");
+            }
+        }
+
+
+        /// <summary>
+        /// 地理位置
+        /// </summary>
+        public class Location
+        {
+            public string Country { get; private set; }
+            public string Region { get; private set; }
+            public string City { get; private set; }
+            public string Isp { get; private set; }
+
+            public Location() { }
+
+            public Location(string c, string r, string ci, string i)
+            {
+                Country = c; Region = r; City = ci; Isp = i;
+            }
+
+            public override string ToString()
+            {
+                return Country + Region + ((Region == City) ? "" : City);
+            }
+
+            private string FixStr(string str)
+            {
+                if (str == "XX") return "";
+                else return str;
+            }
+        }
+        /// <summary>
+        /// 系统信息
+        /// </summary>
+        public class SystemInfo
+        {
+            public string UserName;
+            public string MachineName;
+            public string OS;
+            public int ProcessorCount;
+            public bool Is64;
+            public int MemorySizeInGB;
+
+            public override string ToString()
+            {
+                return
+                    "UserName            " + UserName + "\n" +
+                    "MachineName       " + MachineName + "\n" +
+                    "OS                      " + OS + "\n" +
+                    "ProcessorCount    " + ProcessorCount + "\n" +
+                    "Is64                    " + Is64 + "\n" +
+                    "MemorySize(GB)  " + MemorySizeInGB + "\n";
+
+            }
+        }
+        /// <summary>
+        /// 进程信息
+        /// </summary>
+        public class ProcessInfo
+        {
+            private List<string> process = new List<string>();
+
+            public void Add(string name)
+            {
+                if (!process.Contains(name)) process.Add(name);
+            }
+            public bool IsContain(string name)
+            {
+                return process.Contains(name);
+            }
+        }
+        
+
+        private static string ipcache = null;
+        private static Location locationcache = null;
+        private static SystemInfo sysinfocache = null;
+        
+
+
+        /// <summary>
+        /// Get IP
+        /// </summary>
+        /// <returns></returns>
+        public static string GetIP()
+        {
+            if (ipcache != null) return ipcache;
+            try
+            {
+                WebRequest wr = WebRequest.Create("http://ip.chinaz.com/getip.aspx");
+                Stream s = wr.GetResponse().GetResponseStream();
+                StreamReader sr = new StreamReader(s, Encoding.Default);
+                ipcache = SubStringFromTo(sr.ReadToEnd(), "ip:\'", "\'");
+                sr.Close();
+                s.Close();
+            }
+            catch { ipcache = null; }
+            return ipcache;
+        }
+        /// <summary>
+        /// 获取地理位置
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        public static Location GetLocation()
+        {
+            if (locationcache != null) return locationcache;
+            try
+            {
+                WebRequest wr = WebRequest.Create("http://ip.taobao.com/service/getIpInfo.php?ip=" + GetIP());
+                Stream s = wr.GetResponse().GetResponseStream();
+                StreamReader sr = new StreamReader(s, Encoding.Default);
+                string str = sr.ReadToEnd();
+
+
+                locationcache = new Location(SubStringFromTo(str, "country\":\"", "\""),
+                    SubStringFromTo(str, "region\":\"", "\""),
+                    SubStringFromTo(str, "city\":\"", "\""),
+                    SubStringFromTo(str, "isp\":\"", "\"")
+                    );
+
+                sr.Close();
+                s.Close();
+            }
+            catch { locationcache = null; }
+
+            return locationcache;
+        }
+        /// <summary>
+        /// 获取系统信息
+        /// </summary>
+        /// <returns></returns>
+        public static SystemInfo GetSystemInfo()
+        {
+            if (sysinfocache != null) return sysinfocache;
+            try
+            {
+                sysinfocache = new SystemInfo();
+                sysinfocache.UserName = Environment.UserName;
+                sysinfocache.MachineName = Environment.MachineName;
+                sysinfocache.OS = GetWindowsNameFromVersion(Environment.OSVersion.VersionString);
+                sysinfocache.ProcessorCount = Environment.ProcessorCount;
+                sysinfocache.Is64 = Environment.Is64BitOperatingSystem;
+                sysinfocache.MemorySizeInGB = (int)(new PerformanceCounter("Mono Memory", "Total Physical Memory").RawValue / 1000 / 1000 / 1000);
+
+                if (sysinfocache.OS.Contains("10"))
+                {
+                    string realname = GetWin10UserName();
+                    if (realname != null) sysinfocache.UserName = realname;
+                }
+
+            }
+            catch { sysinfocache = null; }
+
+            return sysinfocache;
+        }
+        /// <summary>
+        /// 获取进程信息
+        /// </summary>
+        /// <returns></returns>
+        public static ProcessInfo GetProcessInfo()
+        {
+            ProcessInfo pi = new ProcessInfo();
+
+            foreach (Process ps in Process.GetProcesses())
+            {
+                pi.Add(ps.ProcessName);
+            }
+            return pi;
+        }
+        /// <summary>
+        /// win10的用户名获取
+        /// </summary>
+        /// <returns></returns>
+        public static string GetWin10UserName()
+        {
+            return Task.RunProcess("WinAccountName.exe");
+        }
+        /// <summary>
+        /// 将版本号转换为系统名
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        private static string GetWindowsNameFromVersion(string v)
+        {
+            const string NamePrefix = "Microsoft Windows NT ";
+
+            string name = "windows ";
+
+            if (v.Contains(NamePrefix + "10")) return name + "10";
+            if (v.Contains(NamePrefix + "6.3")) return name + "8.1";
+            if (v.Contains(NamePrefix + "6.2")) return name + "8";
+            if (v.Contains(NamePrefix + "6.1")) return name + "7";
+            if (v.Contains(NamePrefix + "6.0")) return name + "vista";
+            if (v.Contains(NamePrefix + "5")) return name + "xp";
+            return name;
+        }
+        private static string SubStringFromTo(string str, string frm, string to)
+        {
+            int sp = str.IndexOf(frm) + frm.Length;
+            int ep = str.Substring(sp).IndexOf(to);
+            return str.Substring(sp, ep);
+        }
+    }
+}
